4e42db2a6cb710332a28748e9526200b98d74936
Daehyung Lee <daehyung.lee@cdnetworks.com>
2017-07-17 15:23:28 +0900

    a group development tools should be installed before run bootstrap script

--- bootstrap-puppet.sh
+++ bootstrap-puppet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum -y install @development
 yum -y install puppet-agent
 /opt/puppetlabs/bin/puppet agent --test --waitforcert 10 --onetime
