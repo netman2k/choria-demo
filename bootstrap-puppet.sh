97455348b89b7100bb99da6bd4aab753744e46f6
Daehyung Lee <daehyung.lee@cdnetworks.com>
2017-07-18 15:18:53 +0900

    adds hiera 5

--- bootstrap-puppet.sh
+++ bootstrap-puppet.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 rpm -Uvh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
 yum -y install @development
-# The NATS module needs the systemd related unit packages
-[ $(hostname) = "nats" ] && yum install -y systemd-resolved systemd-networkd
 yum -y install puppet-agent
 /opt/puppetlabs/bin/puppet agent --test --waitforcert 10 --onetime
